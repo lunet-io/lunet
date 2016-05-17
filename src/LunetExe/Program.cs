345d55092f8b682b28ea14f3b03756f2a637deab
Alexandre Mutel <alexandre_mutel@live.com>
2016-05-17 21:25:30 +0900

    Cleanup test main lunet.exe

--- src/LunetExe/Program.cs
+++ src/LunetExe/Program.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.IO;
-using System.IO.Compression;
 using Lunet.Core;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 
 namespace Lunet
@@ -16,54 +12,28 @@ namespace Lunet
     {
         public void Configure(IApplicationBuilder app)
         {
-            app.UseFileServer(new FileServerOptions
-            {
-                FileProvider = new PhysicalFileProvider(@"C:\Code\lunet-io\lunet\doc\.lunet\_site")                
-            });
-
-            //app.UseStaticFiles(new StaticFileOptions()
-            //{
-            //    FileProvider = new PhysicalFileProvider(@"C:\Code\lunet-io\lunet\doc\_site")
-            //});
+            app.UseFileServer(new FileServerOptions());
         }
-
 
         static void Main(string[] args)
         {
             var loggerFactory = new LoggerFactory().AddConsole(LogLevel.Trace);
 
             var site = SiteFactory.FromFile(Path.Combine(Environment.CurrentDirectory, args[0]), loggerFactory);
-
-            //site.Initialize();
-
-            //site.Resources.TryInstall("jquery", "2.2.3");
-
-
             site.Generate();
 
             site.Statistics.Dump((s => site.Info(s)));
 
-
-            //var webListener = new WebListener(loggerFactory);
-            //webListener.Start();
-
-            var directoryName = Path.GetDirectoryName(Path.Combine(Environment.CurrentDirectory, args[0]));
-
-            //WebListener webListener = new WebListener(loggerFactory);
-            //FeatureCollection features = new FeatureCollection();
-            //features.Set<WebListener>(webListener);
-            //features.Set<MessagePump>(new MessagePump(webListener, this._loggerFactory));
-            //features.Set<IServerAddressesFeature>(expr_11, this.SplitAddresses(configuration));
             var host = new WebHostBuilder()
                 //.UseServer("Microsoft.AspNetCore.Server.WebListener")
                 //.UseLoggerFactory(loggerFactory)
                 .UseServer("Microsoft.AspNetCore.Server.Kestrel")
-                .UseContentRoot(directoryName)
+                .UseContentRoot(site.OutputDirectory)
+                .UseWebRoot(site.OutputDirectory)
                 .UseUrls("http://localhost:5001")
                 .UseStartup<Program>()
                 .Build();
             host.Run();
-            //Microsoft.AspNet.Hosting.WebApplication
         }
     }
 }
