f6174b9fdaf79f86a989ecd0f8be909ff3926e89
Alexandre Mutel <alexandre_mutel@live.com>
2020-07-25 08:46:20 +0200

    Add PANEL functions

--- src/Lunet.Core/Core/HelperObject.cs
+++ src/Lunet.Core/Core/HelperObject.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using System.Text;
+using Lunet.Scripts;
 using Scriban.Runtime;
 using Zio;
 
@@ -13,6 +14,31 @@ namespace Lunet.Core
             parent.SetValue(SiteVariables.Helpers, this, true);
             Head = parent.Scripts.CompileAnonymous("include 'builtins/head.sbn-html'");
             SetValue("urlsite", DelegateCustomFunction.CreateFunc((Func<string, string>)Urlsite), true);
+
+            // Helpers used for declaring panels (e.g {{NOTE do}}This is a note.{{end}}
+            var helpers = @"
+# Defines the generic panel helper function
+func PANEL
+    `<div class='` + $0 + `'>`
+        `<div class='` + $0 + `-heading'>`
+            `<i class='` + $0 + `-icon'></i><span class='` + $0 + `-heading-text'></span>`
+        `</div>`
+        `<div class='` + $0 + `-content'>` + '\n\n'
+            $1
+        `</div>`
+    '</div>\n\n'
+end
+
+# Defines panel functions
+func NOTE; PANEL 'lunet-block-note' @$0; end
+func TIP; PANEL 'lunet-block-tip' @$0; end
+func WARNING; PANEL 'lunet-block-warning' @$0; end
+func IMPORTANT; PANEL 'lunet-block-important' @$0; end
+func CAUTION; PANEL 'lunet-block-caution' @$0; end
+func CALLOUT; PANEL 'lunet-block-callout' @$0; end
+";
+            
+            parent.Scripts.TryImportScript(helpers, "internal_helpers", this, ScriptFlags.AllowSiteFunctions, out _);
         }
 
         public object Head
