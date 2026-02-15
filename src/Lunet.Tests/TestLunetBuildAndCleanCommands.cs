3ac37d3cdae831964f01dd741ef2a4da14aae23f
Alexandre Mutel <alexandre_mutel@live.com>
2026-02-15 15:09:17 +0100

    Add Lunet build and clean integration tests

---
+++ src/Lunet.Tests/TestLunetBuildAndCleanCommands.cs
@@ -0,0 +1,62 @@
+// Copyright (c) Alexandre Mutel. All rights reserved.
+// This file is licensed under the BSD-Clause 2 license.
+// See the license.txt file in the project root for more information.
+
+using System.Threading.Tasks;
+using Lunet.Tests.Infrastructure;
+
+namespace Lunet.Tests;
+
+public class TestLunetBuildAndCleanCommands
+{
+    [Test]
+    public async Task TestBuildFailsWhenConfigIsMissing()
+    {
+        var context = new LunetAppTestContext();
+
+        var exitCode = await context.RunAsync("--input-dir=site", "build");
+
+        Assert.AreEqual(1, exitCode);
+    }
+
+    [Test]
+    public async Task TestBuildCopiesStaticFilesToDefaultOutput()
+    {
+        var context = new LunetAppTestContext();
+        context.WriteAllText(
+            "/site/config.scriban",
+            """
+            baseurl = "https://example.com"
+            """);
+        context.WriteAllText("/site/assets/app.js", "console.log('ok');");
+
+        var exitCode = await context.RunAsync("--input-dir=site", "build");
+
+        Assert.AreEqual(0, exitCode);
+        Assert.IsTrue(context.FileExists("/site/.lunet/build/www/assets/app.js"));
+        Assert.AreEqual("console.log('ok');", context.ReadAllText("/site/.lunet/build/www/assets/app.js"));
+    }
+
+    [Test]
+    public async Task TestCleanDeletesBuildFolderWhenConfigIsMissing()
+    {
+        var context = new LunetAppTestContext();
+        context.WriteAllText("/site/.lunet/build/www/old.txt", "stale");
+
+        var exitCode = await context.RunAsync("--input-dir=site", "clean");
+
+        Assert.AreEqual(0, exitCode);
+        Assert.IsFalse(context.FileExists("/site/.lunet/build/www/old.txt"));
+    }
+
+    [Test]
+    public async Task TestCleanFailsWhenConfigExists()
+    {
+        var context = new LunetAppTestContext();
+        context.WriteAllText("/site/config.scriban", "");
+
+        var exitCode = await context.RunAsync("--input-dir=site", "clean");
+
+        Assert.AreEqual(1, exitCode);
+    }
+}
